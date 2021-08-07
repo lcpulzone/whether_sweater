8d6f7609e1ab914290da20ac14588d98ad038db5
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-07 16:02:18 -0700

    Add in vcr

--- spec/api/v1/services/map_service_spec.rb
+++ spec/api/v1/services/map_service_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Map Service Spec' do
+  # before :each do
+  #   VCR.turn_on!
+  # end
+
   describe 'response' do
     it 'can get a response' do
-        
+      # VCR.use_cassette 'map_response' do
+        expect(MapService.latlng).to eq(10)
+      # end
     end
   end
 end
