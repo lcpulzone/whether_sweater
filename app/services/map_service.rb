640deb14984390c803102bea2194ac59ced31b43
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-07 17:12:22 -0700

    Create initial method to call api

---
+++ app/services/map_service.rb
@@ -0,0 +1,16 @@
+class MapService
+  def self.latlng(location)
+    response = conn.get("/geocoding/v1/address?key=#{ENV['MQD_API_KEY']}&location=#{location}")
+    test = parse_json(response)
+    require "pry";binding.pry
+  end
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+private
+  def self.conn
+    Faraday.new(url: 'http://www.mapquestapi.com')
+  end
+end
