3de20c928ca73cae3e61e8b73535b1876a0a5a96
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-07 19:39:03 -0700

    Move WebMock and VCR into a before each block

--- spec/api/v1/services/map_service_spec.rb
+++ spec/api/v1/services/map_service_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Map Service Spec' do
   before :each do
+    # WebMock.allow_net_connect!
+    # VCR.turn_off!
     VCR.turn_on!
   end
 
   describe 'response' do
     it 'can get a response' do
-      # WebMock.allow_net_connect!
-      # VCR.turn_off!
       VCR.use_cassette 'bend_latlng_response' do
         actual = MapService.latlng("bend,or")
+
         expect(actual.class).to eq(Hash)
         expect(actual[:results][0][:locations][0].size).to eq(22)
         expect(actual[:results][0][:locations][0][:latLng].class).to eq(Hash)
