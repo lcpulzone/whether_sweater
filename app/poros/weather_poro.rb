10c16d8a4c00460308b851e242775c85f03ed19b
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-08 09:14:50 -0700

    Add in correct attr_readers, add in formatted time

--- app/poros/weather_poro.rb
+++ app/poros/weather_poro.rb
@@ -1,10 +1,10 @@
 class WeatherPoro
-  attr_reader :sunrise, :sunset, :temp, :feels_like, :humidity, :uvi, :visibility, :conditions, :icon
+  attr_reader :datetime, :sunrise, :sunset, :temperature, :feels_like, :humidity, :uvi, :visibility, :conditions, :icon
 
   def initialize(current_weather)
-    @datetime = Time.new.strftime("%F")
-    @sunrise = current_weather[:current][:sunrise]
-    @sunset = current_weather[:current][:sunset]
+    @datetime = Time.at(current_weather[:current][:dt]).strftime("%B %d, %I:%M %p %z")
+    @sunrise = Time.at(current_weather[:current][:sunrise]).strftime("%B %d, %I:%M %p %z")
+    @sunset = Time.at(current_weather[:current][:sunset]).strftime("%B %d, %I:%M %p %z")
     @temperature = current_weather[:current][:temp]
     @feels_like = current_weather[:current][:feels_like]
     @humidity = current_weather[:current][:humidity]
@@ -14,3 +14,6 @@ class WeatherPoro
     @icon = current_weather[:current][:weather][0][:icon]
   end
 end
+# @datetime = Time.at(current_weather[:current][:dt]).to_s
+# @sunrise = Time.at(current_weather[:current][:sunrise]).to_s
+# @sunset = Time.at(current_weather[:current][:sunset]).to_s
