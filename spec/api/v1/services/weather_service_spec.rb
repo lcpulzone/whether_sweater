32e7b88eb3b48b90a2dc96b31e0d2944c645bee5
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-07 20:36:58 -0700

    Comment in VCR use

--- spec/api/v1/services/weather_service_spec.rb
+++ spec/api/v1/services/weather_service_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Weather Service' do
   before :each do
-    WebMock.allow_net_connect!
-    VCR.turn_off!
-    # VCR.turn_on!
+    # WebMock.allow_net_connect!
+    # VCR.turn_off!
+    VCR.turn_on!
   end
 
   describe 'initial response' do
     it 'can get a response with ' do
-      # VCR.use_cassette 'weather_response' do
+      VCR.use_cassette 'weather_response' do
         actual = WeatherService.find_current_daily_hourly_weather(44.058088, -121.31515)
 
         expect(actual.class).to eq(Hash)
@@ -17,7 +17,7 @@ RSpec.describe 'Weather Service' do
         expect(actual[:hourly].length).to eq(48)
         expect(actual[:daily].length).to eq(8)
         expect(actual[:minutely])
-      # end
+      end
     end
   end
 end
