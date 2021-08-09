5e5da1d323e221d502ece94c191db144bd419893
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-08 18:18:53 -0700

    Add to forecast spec, continue to make more robust

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -2,20 +2,46 @@ require 'rails_helper'
 
 RSpec.describe 'Forecast Controller' do
   before :each do
-    WebMock.allow_net_connect!
-    VCR.turn_off!
-    # VCR.turn_on!
+    # WebMock.allow_net_connect!
+    # VCR.turn_off!
+    VCR.turn_on!
   end
 
   describe 'forecast details' do
-    it 'can give the current, daily, and hourly weather' do
-      # VCR.use_cassette 'forecast_poro' do
-      get api_v1_forecast_path(location:'bend,or')
+    it 'can give a json response for a given location' do
+      VCR.use_cassette 'request_bend_response' do
+        get '/api/v1/forecast?location=bend,or'
 
-      response = JSON.parse(response.body, symbolize_names: true)
-      require "pry";binding.pry
-      expect(response).to be_successful
-      # end
+        forecast_details = JSON.parse(response.body, symbolize_names: true)
+
+        expect(forecast_details[:data].size).to eq(3)
+        expect(forecast_details[:data][:id]).to eq(nil)
+        expect(forecast_details[:data][:type]).to eq("forecast")
+        expect(forecast_details[:data][:attributes].size).to eq(3)
+      end
+    end
+
+    it 'can give the current weather in a json response' do
+      VCR.use_cassette 'request_bend_response' do
+        get '/api/v1/forecast?location=bend,or'
+
+        forecast_details = JSON.parse(response.body, symbolize_names: true)
+
+        expect(forecast_details[:data][:attributes][:current_weather].size).to eq(10)
+        expect(forecast_details[:data][:attributes][:current_weather][:datetime].class).to eq(String)
+        expect(forecast_details[:data][:attributes][:current_weather][:temperature].class).to eq(Float)
+      end
+    end
+
+    it 'current weather does not have these attributes in the json response' do
+      VCR.use_cassette 'request_bend_response' do
+        get '/api/v1/forecast?location=bend,or'
+
+        forecast_details = JSON.parse(response.body, symbolize_names: true)
+
+        expect(forecast_details[:data][:attributes][:current_weather][:pressure].nil?).to eq(true)
+        expect(forecast_details[:data][:attributes][:current_weather][:dew_point].nil?).to eq(true)
+      end
     end
   end
 end
