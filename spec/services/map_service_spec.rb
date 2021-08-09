c20acb40ca13f2f43fca03d6add7a22bffb8504c
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-09 08:12:51 -0700

    Add testing expects

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'Map Service Spec' do
         actual = MapService.latlng("bend,or")
 
         expect(actual.class).to eq(Hash)
-        expect(actual[:results][0][:locations][0].size).to eq(22)
+
         expect(actual[:results][0][:locations][0][:latLng].class).to eq(Hash)
         expect(actual[:results][0][:locations][0][:latLng][:lat]).to eq(44.058088)
         expect(actual[:results][0][:locations][0][:latLng][:lng]).to eq(-121.31515)
