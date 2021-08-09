b32351bd803f97350f699af5c586aa4a10e52441
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-09 09:18:28 -0700

    Add expects to facade spec, create breweries spec

--- spec/facades/breweries_facade_spec.rb
+++ spec/facades/breweries_facade_spec.rb
@@ -7,9 +7,19 @@ RSpec.describe 'Map Service Spec' do
     # VCR.turn_on!
   end
 
-  it 'can list breweries in a location' do
-    actual = BreweriesFacade.list_of_breweries_by_location(location)
+  it 'can list breweries in a location with proper attributes' do
+    actual = BreweriesFacade.list_of_breweries_by_location('bend,or')
 
-    expect(actual.class).to eq(BreweriesPoro)
+    expect(actual.class).to eq(Array)
+    expect(actual[0].class).to eq(BreweriesPoro)
+    expect(actual[0].brewery_type.present?).to eq(true)
+    expect(actual[0].name.present?).to eq(true)
+    expect(actual[0].id.present?).to eq(true)
+  end
+
+  xit 'can list breweries in a location without unneeded attributes' do
+    actual = BreweriesFacade.list_of_breweries_by_location('bend,or')
+
+    expect(actual[0].obdb_id.present?).to_not eq(true)
   end
 end
