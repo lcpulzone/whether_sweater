4e74ab0bbe9f290ab7671208420ae712a04bc09b
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-08 18:16:13 -0700

    Add in proper param

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::ForecastsController < ApplicationController
   def index
     location = params[:location]
-    
-    weather = WeatherFacade.current_daily_hourly_weather()
+    weather = WeatherFacade.current_daily_hourly_weather(location)
     render json: ForecastSerializer.new(weather)
   end
 end
