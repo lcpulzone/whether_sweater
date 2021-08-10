5d4cdeb35531f2ff8270061644cf2160d4767398
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-09 22:26:27 -0700

    Create initial testing file

---
+++ spec/requests/api/v1/user_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'User Resquest' do
+  before :each do
+    WebMock.allow_net_connect!
+    VCR.turn_off!
+    # VCR.turn_on!
+  end
+
+  describe 'Happy Path' do
+    it 'can give a valid response' do
+      user_info = {
+        'email': 'hawkthedog@pets.com'
+        'password': 'helovesfood'
+        'password_confirmation': 'helovesfood'
+      }
+
+      post api_v1_users_path, params: user_info
+
+      
+    end
+  end
+end
