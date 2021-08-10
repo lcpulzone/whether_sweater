2ad160552f200043aa6f5bd57bfdc4248e17d31d
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-09 22:15:53 -0700

    Update routes to have a users create

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecasts#index'
       get '/backgrounds', to: 'backgrounds#index'
+      post '/users', to: 'users#create'
     end
   end
 end
