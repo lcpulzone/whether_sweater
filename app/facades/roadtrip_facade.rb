064c65c9489f7aa4369e4bcff7922201f73a6885
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-10 19:09:35 -0700

    Change method name for developer empathy

--- app/facades/roadtrip_facade.rb
+++ app/facades/roadtrip_facade.rb
@@ -1,7 +1,7 @@
 class RoadtripFacade
   def self.roadtrip_info(origin, destination)
     raw_roadtrip_data = MapService.directions_to_and_from(origin, destination)
-    raw_weather_data = WeatherFacade.weather_on_arrival(destination)
-    test = RoadtripPoro.new(origin, destination, raw_roadtrip_data, raw_weather_data)
+    raw_weather_data = WeatherFacade.find_weather_for_latlng(destination)
+    RoadtripPoro.new(origin, destination, raw_roadtrip_data, raw_weather_data)
   end
 end
--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,13 +1,10 @@
 class WeatherFacade
   def self.current_daily_hourly_weather(location)
-    locate = MapService.latlng(location)
-    lat = locate[:results][0][:locations][0][:latLng][:lat]
-    lng = locate[:results][0][:locations][0][:latLng][:lng]
-    data = WeatherService.find_current_daily_hourly_weather(lat, lng)
+    data = self.find_weather_for_latlng(location)
     ForecastPoro.new(data)
   end
 
-  def self.weather_on_arrival(location)
+  def self.find_weather_for_latlng(location)
     locate = MapService.latlng(location)
     lat = locate[:results][0][:locations][0][:latLng][:lat]
     lng = locate[:results][0][:locations][0][:latLng][:lng]
