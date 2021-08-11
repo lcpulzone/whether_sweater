84e9d2288e300309cc8f9a7723c92565388dd3b9
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-10 18:11:19 -0700

    Create roadtrip facade, adjust weather facade

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -6,4 +6,11 @@ class WeatherFacade
     data = WeatherService.find_current_daily_hourly_weather(lat, lng)
     ForecastPoro.new(data)
   end
+
+  def self.weather_on_arrival(location)
+    locate = MapService.latlng(location)
+    lat = locate[:results][0][:locations][0][:latLng][:lat]
+    lng = locate[:results][0][:locations][0][:latLng][:lng]
+    data = WeatherService.find_current_daily_hourly_weather(lat, lng)
+  end
 end
