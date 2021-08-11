ffade2305c9793a32a6662c0be4e03052e663429
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-10 19:09:55 -0700

    Refactor method to be more concise

--- app/controllers/api/v1/roadtrips_controller.rb
+++ app/controllers/api/v1/roadtrips_controller.rb
@@ -1,13 +1,12 @@
 class Api::V1::RoadtripsController < ApplicationController
   def create
     user = User.find_by(api_key: params[:api_key])
+    trip = RoadtripFacade.roadtrip_info(params[:origin], params[:destination])
+
     if user.nil? == false
-      origin = params[:origin]
-      destination = params[:destination]
-      trip = RoadtripFacade.roadtrip_info(origin, destination)
       test = render json: RoadtripsSerializer.new(trip), status: 200
-    else
-      render json: { error: "Woah, you can't drive there. Can I suggest traveling via boat or plane?" }
+    elsif user.nil?
+      render json: { error: "You are not authorized" }, status: 401
     end
   end
 end
