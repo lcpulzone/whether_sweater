84490d3e03b983a082a76585cca111e2860eba99
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-09-19 09:19:39 -0700

    Remove unnecessary space

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecasts#index'
