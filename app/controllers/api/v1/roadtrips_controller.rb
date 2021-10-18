fd98f865eb3c7d6d16a552bb54660e5b368ba182
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-10-18 11:45:42 -0700

    Remove instance variable that was used for testing purposes only

--- app/controllers/api/v1/roadtrips_controller.rb
+++ app/controllers/api/v1/roadtrips_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::RoadtripsController < ApplicationController
     trip = RoadtripFacade.roadtrip_info(params[:origin], params[:destination])
 
     if user.nil? == false
-      test = render json: RoadtripsSerializer.new(trip), status: 200
+      render json: RoadtripsSerializer.new(trip), status: 200
     elsif user.nil?
       render json: { error: "You are not authorized" }, status: 401
     end
