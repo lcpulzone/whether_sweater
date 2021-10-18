ffda523637aad3ec1ed914463694f1c56ff1fe14
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-10-18 11:49:29 -0700

    Add space betwee instance variables and conditionals for consistency

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     found_user = User.find_by(email: params[:email])
+    
     if found_user && found_user.authenticate(params[:password])
       session[:user_id] = found_user.id
       render json: UsersSerializer.new(found_user), status: 200
